1737d9fa288a67093bb7860cea2aceb802c5cd55
suntre <kpietka00@gmail.com>
2023-01-04 18:38:55 +0100

    check if service is already running

--- Application/app/src/main/java/com/example/application/Activities/MainActivity.java
+++ Application/app/src/main/java/com/example/application/Activities/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.core.app.TaskStackBuilder;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.ActivityManager;
 import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -87,7 +88,10 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         dismissNotification();
-        startWaterNotificationService();
+        if(!isServiceRunning(NotifyAboutWater.class)){
+            startWaterNotificationService();
+        }
+
         createNotificationChannel();
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_FILE_NAME, MODE_PRIVATE);
         dailyGlassesOfWater = sharedPreferences.getInt(WATER_GLASSES_KEY, 0);
@@ -373,9 +377,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private boolean isServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private void startWaterNotificationService(){
         Intent intent = new Intent(this, NotifyAboutWater.class);
-        startService(intent);
+            startService(intent);
     }
 
     private void startStepCounterService() {
